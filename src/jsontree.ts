92f0328ca352f4eba4ffc1b456f2232c28837b81
William Troup <williamtroup@gmail.com>
2024-07-08 22:52:09 +0100

    Add start of structure for main file.

---
+++ src/jsontree.ts
@@ -0,0 +1,31 @@
+/**
+ * JsonTree.js
+ * 
+ * A lightweight JavaScript library that generates customizable tree views to better visualize JSON data.
+ * 
+ * @file        jsontree.ts
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+import { type Configuration } from "./ts/type";
+
+
+( () => {
+    // Variables: Configuration
+    let _configuration: Configuration = {} as Configuration;
+
+    
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Initialize JsonTree.js
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    ( () => {
+
+    } )();
+} )();
