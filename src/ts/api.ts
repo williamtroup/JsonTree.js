4726727e2faf2b6baffd6fbc3536523362af816f
William Troup <williamtroup@gmail.com>
2024-07-08 22:48:51 +0100

    Fixed API.

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,3 +1,19 @@
+/**
+ * JsonTree.js
+ * 
+ * A lightweight JavaScript library that generates customizable tree views to better visualize JSON data.
+ * 
+ * @file        api.ts
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+import { type Configuration } from "./type";
+
+
 export type PublicApi = {
     /*
      * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
