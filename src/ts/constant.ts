7651c33f23787b8510fb6b4b860e3d0e828d1f9f
William Troup <williamtroup@gmail.com>
2024-07-08 22:35:05 +0100

    Added constant and enum TS files.

---
+++ src/ts/constant.ts
@@ -0,0 +1,16 @@
+/**
+ * JsonTree.js
+ * 
+ * A lightweight JavaScript library that generates customizable tree views to better visualize JSON data.
+ * 
+ * @file        constant.ts
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+export namespace Constants {
+    export const JSONTREE_JS_ATTRIBUTE_NAME = "data-jsontree-js";
+}
---
+++ src/ts/enum.ts
@@ -0,0 +1,17 @@
+/**
+ * JsonTree.js
+ * 
+ * A lightweight JavaScript library that generates customizable tree views to better visualize JSON data.
+ * 
+ * @file        enum.ts
+ * @version     v2.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+export const enum Char {
+    empty = "",
+    space = " ",
+}
