a8ccff471e25b10a5e427227a72b5b2b0b9df463
William Troup <williamtroup@gmail.com>
2024-07-09 12:22:07 +0100

    Added core of private functions.

--- dist/jsontree.js
+++ dist/jsontree.js
@@ -1,1 +1,254 @@
-"use strict";//# sourceMappingURL=jsontree.js.map
+"use strict";
+
+var Is;
+
+(e => {
+    function t(e) {
+        return e !== null && e !== void 0 && e.toString() !== "";
+    }
+    e.defined = t;
+    function n(e) {
+        return t(e) && typeof e === "object";
+    }
+    e.definedObject = n;
+    function o(e) {
+        return t(e) && typeof e === "boolean";
+    }
+    e.definedBoolean = o;
+    function r(e) {
+        return t(e) && typeof e === "string";
+    }
+    e.definedString = r;
+    function a(e) {
+        return t(e) && typeof e === "function";
+    }
+    e.definedFunction = a;
+    function l(e) {
+        return t(e) && typeof e === "number";
+    }
+    e.definedNumber = l;
+    function u(e) {
+        return n(e) && e instanceof Array;
+    }
+    e.definedArray = u;
+    function i(e) {
+        return n(e) && e instanceof Date;
+    }
+    e.definedDate = i;
+    function s(e) {
+        return t(e) && typeof e === "number" && e % 1 !== 0;
+    }
+    e.definedDecimal = s;
+    function f(e, t = 1) {
+        return !u(e) || e.length < t;
+    }
+    e.invalidOptionArray = f;
+})(Is || (Is = {}));
+
+var Data;
+
+(e => {
+    let t;
+    (e => {
+        function t() {
+            const e = [];
+            for (let t = 0; t < 32; t++) {
+                if (t === 8 || t === 12 || t === 16 || t === 20) {
+                    e.push("-");
+                }
+                const n = Math.floor(Math.random() * 16).toString(16);
+                e.push(n);
+            }
+            return e.join("");
+        }
+        e.newGuid = t;
+        function n(e, t = 1) {
+            const n = e.toString();
+            let o = n;
+            if (n.length < t) {
+                const e = t - n.length + 1;
+                o = Array(e).join("0") + n;
+            }
+            return o;
+        }
+        e.padNumber = n;
+    })(t = e.String || (e.String = {}));
+    function n(e, t) {
+        return typeof e === "string" ? e : t;
+    }
+    e.getDefaultAnyString = n;
+    function o(e, t) {
+        return Is.definedString(e) ? e : t;
+    }
+    e.getDefaultString = o;
+    function r(e, t) {
+        return Is.definedBoolean(e) ? e : t;
+    }
+    e.getDefaultBoolean = r;
+    function a(e, t) {
+        return Is.definedNumber(e) ? e : t;
+    }
+    e.getDefaultNumber = a;
+    function l(e, t) {
+        return Is.definedFunction(e) ? e : t;
+    }
+    e.getDefaultFunction = l;
+    function u(e, t) {
+        return Is.definedArray(e) ? e : t;
+    }
+    e.getDefaultArray = u;
+    function i(e, t) {
+        return Is.definedObject(e) ? e : t;
+    }
+    e.getDefaultObject = i;
+    function s(e, t) {
+        let n = t;
+        if (Is.definedString(e)) {
+            const o = e.toString().split(" ");
+            if (o.length === 0) {
+                e = t;
+            } else {
+                n = o;
+            }
+        } else {
+            n = u(e, t);
+        }
+        return n;
+    }
+    e.getDefaultStringOrArray = s;
+})(Data || (Data = {}));
+
+(() => {
+    let _configuration = {};
+    function buildAttributeOptions(e) {
+        var t = Data.getDefaultObject(e, {});
+        t.data = Data.getDefaultObject(t.data, null);
+        t.showCounts = Data.getDefaultBoolean(t.showCounts, true);
+        t.useZeroIndexingForArrays = Data.getDefaultBoolean(t.useZeroIndexingForArrays, true);
+        t.dateTimeFormat = Data.getDefaultString(t.dateTimeFormat, "{dd}{o} {mmmm} {yyyy} {hh}:{MM}:{ss}");
+        t.showArrowToggles = Data.getDefaultBoolean(t.showArrowToggles, true);
+        t.showStringQuotes = Data.getDefaultBoolean(t.showStringQuotes, true);
+        t.showAllAsClosed = Data.getDefaultBoolean(t.showAllAsClosed, false);
+        t.sortPropertyNames = Data.getDefaultBoolean(t.sortPropertyNames, true);
+        t.sortPropertyNamesInAlphabeticalOrder = Data.getDefaultBoolean(t.sortPropertyNamesInAlphabeticalOrder, true);
+        t.showCommas = Data.getDefaultBoolean(t.showCommas, false);
+        t.reverseArrayValues = Data.getDefaultBoolean(t.reverseArrayValues, false);
+        t.addArrayIndexPadding = Data.getDefaultBoolean(t.addArrayIndexPadding, false);
+        t.showValueColors = Data.getDefaultBoolean(t.showValueColors, true);
+        t.maximumDecimalPlaces = Data.getDefaultNumber(t.maximumDecimalPlaces, 2);
+        t.maximumStringLength = Data.getDefaultNumber(t.maximumStringLength, 0);
+        t.showStringHexColors = Data.getDefaultBoolean(t.showStringHexColors, false);
+        t = buildAttributeOptionTitle(t);
+        t = buildAttributeOptionIgnore(t);
+        t = buildAttributeOptionCustomTriggers(t);
+        return t;
+    }
+    function buildAttributeOptionTitle(e) {
+        e.title = Data.getDefaultObject(e.title, {});
+        e.title.text = Data.getDefaultString(e.title.text, "JsonTree.js");
+        e.title.show = Data.getDefaultBoolean(e.title.show, true);
+        e.title.showTreeControls = Data.getDefaultBoolean(e.title.showTreeControls, true);
+        e.title.showCopyButton = Data.getDefaultBoolean(e.title.showCopyButton, false);
+        return e;
+    }
+    function buildAttributeOptionIgnore(e) {
+        e.ignore = Data.getDefaultObject(e.ignore, {});
+        e.ignore.nullValues = Data.getDefaultBoolean(e.ignore.nullValues, false);
+        e.ignore.functionValues = Data.getDefaultBoolean(e.ignore.functionValues, false);
+        e.ignore.unknownValues = Data.getDefaultBoolean(e.ignore.unknownValues, false);
+        e.ignore.booleanValues = Data.getDefaultBoolean(e.ignore.booleanValues, false);
+        e.ignore.decimalValues = Data.getDefaultBoolean(e.ignore.decimalValues, false);
+        e.ignore.numberValues = Data.getDefaultBoolean(e.ignore.numberValues, false);
+        e.ignore.stringValues = Data.getDefaultBoolean(e.ignore.stringValues, false);
+        e.ignore.dateValues = Data.getDefaultBoolean(e.ignore.dateValues, false);
+        e.ignore.objectValues = Data.getDefaultBoolean(e.ignore.objectValues, false);
+        e.ignore.arrayValues = Data.getDefaultBoolean(e.ignore.arrayValues, false);
+        return e;
+    }
+    function buildAttributeOptionCustomTriggers(e) {
+        e.events = Data.getDefaultObject(e.events, {});
+        e.events.onBeforeRender = Data.getDefaultFunction(e.events.onBeforeRender, null);
+        e.events.onRenderComplete = Data.getDefaultFunction(e.events.onRenderComplete, null);
+        e.events.onValueClick = Data.getDefaultFunction(e.events.onValueClick, null);
+        e.events.onRefresh = Data.getDefaultFunction(e.events.onRefresh, null);
+        e.events.onCopyAll = Data.getDefaultFunction(e.events.onCopyAll, null);
+        e.events.onOpenAll = Data.getDefaultFunction(e.events.onOpenAll, null);
+        e.events.onCloseAll = Data.getDefaultFunction(e.events.onCloseAll, null);
+        e.events.onDestroy = Data.getDefaultFunction(e.events.onDestroy, null);
+        e.events.onBooleanRender = Data.getDefaultFunction(e.events.onBooleanRender, null);
+        e.events.onDecimalRender = Data.getDefaultFunction(e.events.onDecimalRender, null);
+        e.events.onNumberRender = Data.getDefaultFunction(e.events.onNumberRender, null);
+        e.events.onStringRender = Data.getDefaultFunction(e.events.onStringRender, null);
+        e.events.onDateRender = Data.getDefaultFunction(e.events.onDateRender, null);
+        e.events.onFunctionRender = Data.getDefaultFunction(e.events.onFunctionRender, null);
+        e.events.onNullRender = Data.getDefaultFunction(e.events.onNullRender, null);
+        e.events.onUnknownRender = Data.getDefaultFunction(e.events.onUnknownRender, null);
+        return e;
+    }
+    function fireCustomTriggerEvent(e, ...t) {
+        if (Is.definedFunction(e)) {
+            e.apply(null, [].slice.call(t, 0));
+        }
+    }
+    function getObjectFromString(objectString) {
+        const result = {
+            parsed: true,
+            object: null
+        };
+        try {
+            if (Is.definedString(objectString)) {
+                result.object = JSON.parse(objectString);
+            }
+        } catch (e1) {
+            try {
+                result.object = eval("(" + objectString + ")");
+                if (Is.definedFunction(result.object)) {
+                    result.object = result.object();
+                }
+            } catch (e) {
+                if (!_configuration.safeMode) {
+                    console.error(_configuration.objectErrorText.replace("{{error_1}}", e1.message).replace("{{error_2}}", e.message));
+                    result.parsed = false;
+                }
+                result.object = null;
+            }
+        }
+        return result;
+    }
+    const _public = {
+        refresh: function(e) {
+            throw new Error("Function not implemented.");
+        },
+        refreshAll: function() {
+            throw new Error("Function not implemented.");
+        },
+        render: function(e, t) {
+            throw new Error("Function not implemented.");
+        },
+        renderAll: function() {
+            throw new Error("Function not implemented.");
+        },
+        openAll: function(e) {
+            throw new Error("Function not implemented.");
+        },
+        closeAll: function(e) {
+            throw new Error("Function not implemented.");
+        },
+        destroy: function(e) {
+            throw new Error("Function not implemented.");
+        },
+        destroyAll: function() {
+            throw new Error("Function not implemented.");
+        },
+        setConfiguration: function(e) {
+            throw new Error("Function not implemented.");
+        },
+        getIds: function() {
+            throw new Error("Function not implemented.");
+        },
+        getVersion: function() {
+            throw new Error("Function not implemented.");
+        }
+    };
+    (() => {})();
+})();//# sourceMappingURL=jsontree.js.map
--- src/jsontree.ts
+++ src/jsontree.ts
@@ -12,7 +12,14 @@
 
 
 import { PublicApi } from "./ts/api";
-import { type Configuration } from "./ts/type";
+import { Data } from "./ts/data";
+import { Is } from "./ts/is";
+import { BindingOptions, Events, Ignore, Title, type Configuration } from "./ts/type";
+
+type StringToJson = {
+    parsed: boolean;
+    object: any;
+};
 
 
 ( () => {
@@ -20,6 +27,139 @@ import { type Configuration } from "./ts/type";
     let _configuration: Configuration = {} as Configuration;
 
     
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Options
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function buildAttributeOptions( newOptions: any ) : BindingOptions {
+        var options = Data.getDefaultObject( newOptions, {} as BindingOptions );
+        options.data = Data.getDefaultObject( options.data, null! );
+        options.showCounts = Data.getDefaultBoolean( options.showCounts, true );
+        options.useZeroIndexingForArrays = Data.getDefaultBoolean( options.useZeroIndexingForArrays, true );
+        options.dateTimeFormat = Data.getDefaultString( options.dateTimeFormat, "{dd}{o} {mmmm} {yyyy} {hh}:{MM}:{ss}" );
+        options.showArrowToggles = Data.getDefaultBoolean( options.showArrowToggles, true );
+        options.showStringQuotes = Data.getDefaultBoolean( options.showStringQuotes, true );
+        options.showAllAsClosed = Data.getDefaultBoolean( options.showAllAsClosed, false );
+        options.sortPropertyNames = Data.getDefaultBoolean( options.sortPropertyNames, true );
+        options.sortPropertyNamesInAlphabeticalOrder = Data.getDefaultBoolean( options.sortPropertyNamesInAlphabeticalOrder, true );
+        options.showCommas = Data.getDefaultBoolean( options.showCommas, false );
+        options.reverseArrayValues = Data.getDefaultBoolean( options.reverseArrayValues, false );
+        options.addArrayIndexPadding = Data.getDefaultBoolean( options.addArrayIndexPadding, false );
+        options.showValueColors = Data.getDefaultBoolean( options.showValueColors, true );
+        options.maximumDecimalPlaces = Data.getDefaultNumber( options.maximumDecimalPlaces, 2 );
+        options.maximumStringLength = Data.getDefaultNumber( options.maximumStringLength, 0 );
+        options.showStringHexColors = Data.getDefaultBoolean( options.showStringHexColors, false );
+
+        options = buildAttributeOptionTitle( options );
+        options = buildAttributeOptionIgnore( options );
+        options = buildAttributeOptionCustomTriggers( options );
+
+        return options;
+    }
+
+    function buildAttributeOptionTitle( options: BindingOptions ) : BindingOptions {
+        options.title = Data.getDefaultObject( options.title, {} as Title );
+        options.title!.text = Data.getDefaultString( options.title!.text, "JsonTree.js" );
+        options.title!.show = Data.getDefaultBoolean( options.title!.show, true );
+        options.title!.showTreeControls = Data.getDefaultBoolean( options.title!.showTreeControls, true );
+        options.title!.showCopyButton = Data.getDefaultBoolean( options.title!.showCopyButton, false );
+
+        return options;
+    }
+
+    function buildAttributeOptionIgnore( options: BindingOptions ) : BindingOptions {
+        options.ignore = Data.getDefaultObject( options.ignore, {} as Ignore );
+        options.ignore!.nullValues = Data.getDefaultBoolean( options.ignore!.nullValues, false );
+        options.ignore!.functionValues = Data.getDefaultBoolean( options.ignore!.functionValues, false );
+        options.ignore!.unknownValues = Data.getDefaultBoolean( options.ignore!.unknownValues, false );
+        options.ignore!.booleanValues = Data.getDefaultBoolean( options.ignore!.booleanValues, false );
+        options.ignore!.decimalValues = Data.getDefaultBoolean( options.ignore!.decimalValues, false );
+        options.ignore!.numberValues = Data.getDefaultBoolean( options.ignore!.numberValues, false );
+        options.ignore!.stringValues = Data.getDefaultBoolean( options.ignore!.stringValues, false );
+        options.ignore!.dateValues = Data.getDefaultBoolean( options.ignore!.dateValues, false );
+        options.ignore!.objectValues = Data.getDefaultBoolean( options.ignore!.objectValues, false );
+        options.ignore!.arrayValues = Data.getDefaultBoolean( options.ignore!.arrayValues, false );
+
+        return options;
+    }
+
+    function buildAttributeOptionCustomTriggers( options: BindingOptions ) : BindingOptions {
+        options.events = Data.getDefaultObject( options.events, {} as Events );
+        options.events!.onBeforeRender = Data.getDefaultFunction( options.events!.onBeforeRender, null! );
+        options.events!.onRenderComplete = Data.getDefaultFunction( options.events!.onRenderComplete, null! );
+        options.events!.onValueClick = Data.getDefaultFunction( options.events!.onValueClick, null! );
+        options.events!.onRefresh = Data.getDefaultFunction( options.events!.onRefresh, null! );
+        options.events!.onCopyAll = Data.getDefaultFunction( options.events!.onCopyAll, null! );
+        options.events!.onOpenAll = Data.getDefaultFunction( options.events!.onOpenAll, null! );
+        options.events!.onCloseAll = Data.getDefaultFunction( options.events!.onCloseAll, null! );
+        options.events!.onDestroy = Data.getDefaultFunction( options.events!.onDestroy, null! );
+        options.events!.onBooleanRender = Data.getDefaultFunction( options.events!.onBooleanRender, null! );
+        options.events!.onDecimalRender = Data.getDefaultFunction( options.events!.onDecimalRender, null! );
+        options.events!.onNumberRender =Data.getDefaultFunction( options.events!.onNumberRender, null! );
+        options.events!.onStringRender = Data.getDefaultFunction( options.events!.onStringRender, null! );
+        options.events!.onDateRender = Data.getDefaultFunction( options.events!.onDateRender, null! );
+        options.events!.onFunctionRender = Data.getDefaultFunction( options.events!.onFunctionRender, null! );
+        options.events!.onNullRender = Data.getDefaultFunction( options.events!.onNullRender, null! );
+        options.events!.onUnknownRender = Data.getDefaultFunction( options.events!.onUnknownRender, null! );
+
+        return options;
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Triggering Custom Events
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function fireCustomTriggerEvent( triggerFunction: Function, ...args : any[] ) : void {
+        if ( Is.definedFunction( triggerFunction ) ) {
+            triggerFunction.apply( null, [].slice.call( args, 0 ) );
+        }
+    }
+
+
+    /*
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     * Default Parameter/Option Handling
+     * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    function getObjectFromString( objectString: any ) : StringToJson {
+        const result: StringToJson = {
+            parsed: true,
+            object: null
+        } as StringToJson;
+
+        try {
+            if ( Is.definedString( objectString ) ) {
+                result.object = JSON.parse( objectString );
+            }
+
+        } catch ( e1: any ) {
+            try {
+                result.object = eval( "(" + objectString + ")" );
+
+                if ( Is.definedFunction( result.object ) ) {
+                    result.object = result.object();
+                }
+                
+            } catch ( e2: any ) {
+                if ( !_configuration.safeMode ) {
+                    console.error( _configuration.objectErrorText!.replace( "{{error_1}}",  e1.message ).replace( "{{error_2}}",  e2.message ) );
+                    result.parsed = false;
+                }
+                
+                result.object = null;
+            }
+        }
+
+        return result;
+    }
+
+
 	/*
 	 * ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	 * Public API Functions:
