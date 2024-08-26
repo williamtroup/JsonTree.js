df1ede5c5e8ba9653bf9abd94eba97a4b8d1a15f
William Troup <williamtroup@gmail.com>
2024-08-26 23:16:30 +0100

    Updated scripts.

--- test/js/scripts.js
+++ test/js/scripts.js
@@ -28,6 +28,7 @@ function bindingOptions( showValueColors = true, allowValueToolTips = true ) {
         openInFullScreenMode: false,
         enableFullScreenToggling: true,
         useZeroIndexingForArrays: true,
+        showCounts: true,
         valueToolTips: allowValueToolTips ? {
             "value1": "This is a boolean tooltip for Value 1",
             "value5\\1": "This is a string tooltip for Value 1 > Array Index 1",
