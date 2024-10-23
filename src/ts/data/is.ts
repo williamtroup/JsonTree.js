8d2d8ad97e4be1d460a6d64cb9474f18238a8671
William Troup <williamtroup@gmail.com>
2024-10-23 21:44:58 +0100

    Improved the RegEx used to identify Date/Time strings (allowing for timezone adjustments).

--- src/ts/data/is.ts
+++ src/ts/data/is.ts
@@ -38,7 +38,7 @@ export namespace Is {
         }
     
         export function date( dateTimeString: string ) {
-            const regExp: RegExp = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/;
+            const regExp: RegExp = /\d{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2]\d|3[0-1])T(?:[0-1]\d|2[0-3]):[0-5]\d:[0-5]\d(?:\.\d+|)(?:Z|(?:\+|\-)(?:\d{2}):?(?:\d{2}))/;
 
             return dateTimeString.match( regExp );
         }
